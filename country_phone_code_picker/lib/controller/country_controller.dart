33cce6d9b56c414f6d89378377fa274480d7d530
samitkapoor <samitkapoor77@gmail.com>
2022-05-03 19:58:12 +0530

    Added Controllers

---
+++ country_phone_code_picker/lib/controller/country_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:country_phone_code_picker/models/country.dart' as c;
+
+class CountryController extends GetxController {
+  List<c.Country> countryPhoneCodes = [];
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+}
